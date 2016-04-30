55128af9728543e53ff7a8167c0f28200a2e86a9
fenghai <feng_hai@aliyun.com>
2016-04-30 12:46:54 +0800

    文件提交

--- AnalyzeForm/ProtocolManagement.cs
+++ AnalyzeForm/ProtocolManagement.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AnalyzeForm
 {
-    public partial class Form1 : Form
+    public partial class ProtocolManagement : Form
     {
-        public Form1()
+        public ProtocolManagement()
         {
             InitializeComponent();
         }
