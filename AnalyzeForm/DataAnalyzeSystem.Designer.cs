11e6954d22372eba8c852fda8bc62f36fd2149c3
fenghai <feng_hai@aliyun.com>
2016-05-02 10:10:51 +0800

    加载树目录

---
+++ AnalyzeForm/DataAnalyzeSystem.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace AnalyzeForm
+{
+    partial class DataAnalyzeSystem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.protocolManagementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataAnalyzeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.treeView2 = new System.Windows.Forms.TreeView();
+            this.analyzeData = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(764, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.protocolManagementToolStripMenuItem,
+            this.dataAnalyzeToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(39, 21);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // protocolManagementToolStripMenuItem
+            // 
+            this.protocolManagementToolStripMenuItem.Name = "protocolManagementToolStripMenuItem";
+            this.protocolManagementToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.protocolManagementToolStripMenuItem.Text = "ProtocolManagement";
+            this.protocolManagementToolStripMenuItem.Click += new System.EventHandler(this.protocolManagementToolStripMenuItem_Click);
+            // 
+            // dataAnalyzeToolStripMenuItem
+            // 
+            this.dataAnalyzeToolStripMenuItem.Name = "dataAnalyzeToolStripMenuItem";
+            this.dataAnalyzeToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.dataAnalyzeToolStripMenuItem.Text = "DataAnalyze";
+            this.dataAnalyzeToolStripMenuItem.Click += new System.EventHandler(this.dataAnalyzeToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.closeToolStripMenuItem.Text = "close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closseToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(45, 21);
+            this.helpToolStripMenuItem.Text = "help";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "C:\\";
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(198, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "选择地址";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(680, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "选择地址";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(348, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(10, 82);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(261, 371);
+            this.treeView1.TabIndex = 4;
+            // 
+            // treeView2
+            // 
+            this.treeView2.Location = new System.Drawing.Point(485, 82);
+            this.treeView2.Name = "treeView2";
+            this.treeView2.Size = new System.Drawing.Size(267, 371);
+            this.treeView2.TabIndex = 5;
+            // 
+            // analyzeData
+            // 
+            this.analyzeData.Location = new System.Drawing.Point(336, 240);
+            this.analyzeData.Name = "analyzeData";
+            this.analyzeData.Size = new System.Drawing.Size(75, 23);
+            this.analyzeData.TabIndex = 6;
+            this.analyzeData.Text = "解析数据";
+            this.analyzeData.UseVisualStyleBackColor = true;
+            this.analyzeData.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(336, 290);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(284, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "数据协议：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "原始文件：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 21);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(484, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "结果文件：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(540, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 21);
+            this.textBox2.TabIndex = 12;
+            // 
+            // DataAnalyzeSystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 467);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.analyzeData);
+            this.Controls.Add(this.treeView2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "DataAnalyzeSystem";
+            this.Text = "DataAnalyzeSystem";
+            this.Load += new System.EventHandler(this.DataAnalyzeSystem_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem protocolManagementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dataAnalyzeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TreeView treeView2;
+        private System.Windows.Forms.Button analyzeData;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ AnalyzeForm/DataAnalyzeSystem.cs
@@ -0,0 +1,152 @@
+﻿using AnalyzeLibrary.file;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalyzeForm
+{
+    public partial class DataAnalyzeSystem : Form
+    {
+
+
+        public class Info
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+
+        }
+        public DataAnalyzeSystem()
+        {
+            InitializeComponent();
+        }
+
+        private void protocolManagementToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataAnalyzeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void closseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void DataAnalyzeSystem_Load(object sender, EventArgs e)
+        {
+           string [] protocols= DirFileHelper.GetFileNames(System.IO.Directory.GetCurrentDirectory()+@"\resource", "*.xml", true);
+
+            IList<Info> infoList = new List<Info>();
+            foreach (string str in protocols)
+            {
+                Info inf = new Info() { Id = str, Name = DirFileHelper.GetFileNameNoExtension(str) };
+                infoList.Add(inf);
+            };
+
+            comboBox1.DataSource = infoList;
+            comboBox1.ValueMember = "Id";
+            comboBox1.DisplayMember = "Name";
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                string sss = folder.SelectedPath;
+                textBox1.Text = sss;
+                string[] temp = loadFile(sss);
+                TreeNode root = new TreeNode();
+                root.Text = DirFileHelper.GetFileName(sss);
+                root.Tag = sss;
+                treeView1.Nodes.Add(root);
+                InitTree(sss, root);
+                MessageBox.Show(temp.ToString(), "选择文件夹提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("已选择文件夹:" + sss, "选择文件夹提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                string sss = folder.SelectedPath;
+                textBox2.Text = sss;
+                string[] temp = loadFile(sss);
+                TreeNode root = new TreeNode();
+                root.Text = DirFileHelper.GetFileName(sss);
+                root.Tag = sss;
+                treeView2.Nodes.Add(root);
+                InitTree(sss, root);
+                MessageBox.Show("已选择文件夹:" + sss, "选择文件夹提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitTree(string file ,TreeNode node)
+        {
+
+            var temp = loadDirectories(file);
+            foreach (string fileName in temp)
+            {
+               var tempNode= AddNode(node, fileName);
+                InitTree(fileName, tempNode);
+            }
+            if (!DirFileHelper.IsEmptyDirectory(file))
+            {
+                var tempFile = loadFile(file);
+                foreach (string fileN in tempFile)
+                {
+                    AddNode(node, fileN);
+                }
+
+            }
+        }
+        private TreeNode AddNode(TreeNode root  , string fileName)
+        {
+            
+                TreeNode node = new TreeNode();
+                node.Text = DirFileHelper.GetFileName(fileName);
+                node.Tag = fileName;
+                root.Nodes.Add(node);
+                return node;
+             
+            
+        }
+
+        private string[] loadFile(string url)
+        {
+            return DirFileHelper.GetFileNames(url, "*", false);
+        }
+
+        private string[] loadDirectories(string url)
+        {
+            return DirFileHelper.GetDirectories(url, "*", false);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
